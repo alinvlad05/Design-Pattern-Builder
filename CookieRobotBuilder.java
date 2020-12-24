59abd5ac13fe6c4819ff1b17f08980a6f356e4a7
Alin Vlad <alinvlad05@gmail.com>
2020-12-24 16:46:03 +0200

    Initial commit

---
+++ CookieRobotBuilder.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class CookieRobotBuilder implements RobotBuilder {
+    CookieRobotBuildable robot;
+    ArrayList<Integer> actions;
+
+    public CookieRobotBuilder() {
+        robot = new CookieRobotBuildable();
+        actions = new ArrayList<Integer>();
+    }
+
+    public void addStart() {
+        actions.add(new Integer(1));
+    }
+
+    public void addGetParts() {
+        actions.add(new Integer(2));
+    }
+
+    public void addAssemble() {
+        actions.add(new Integer(3));
+    }
+
+    public void addTest() {
+        actions.add(new Integer(4));
+    }
+
+    public void addStop() {
+        actions.add(new Integer(5));
+    }
+
+    public RobotBuildable getRobot() {
+        robot.loadActions(actions);
+        return robot;
+    }
+
+}
